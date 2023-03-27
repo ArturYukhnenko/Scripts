03c10edbda23eaa868bd8787272110ffcb09a0aa
Artur Yukhnenko <787897879uk@gmail.com>
2023-03-27 02:46:10 +0200

    [+] Shop Script added

---
+++ Shop/ShopMain.cs
@@ -0,0 +1,16 @@
+// using System.Collections.Generic;
+// using Unity.Collections;
+//
+// public class ShopMain {
+//     
+//     //Here stored all items with prices per 1 unit
+//     public static Dictionary<Item,float> Prices { get; private set; }
+//     
+//     
+//
+//     public ShopMain(Dictionary<Item, float> prices) {
+//         Prices = prices;
+//     }
+//     
+//     
+// }
