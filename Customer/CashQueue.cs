f826014b113f01029f0412c44ffc27216487a83d
Anton1812 <s21581@pjwstk.edu.pl>
2023-10-22 21:57:09 +0200

    [+] Queue (not yet ready)

--- Customer/CashQueue.cs
+++ Customer/CashQueue.cs
@@ -1,40 +1,128 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class CashQueue : MonoBehaviour
 {
 
-    [SerializeField] Transform[] Positions;
+
+    [SerializeField] globalCustomer gc;
+    [SerializeField] public List<Transform> Positions = new List<Transform>();
     [SerializeField] float ObjectSpeed;
+
+
+
+    public bool pos3Free;
+    public bool pos2Free;
+    public bool pos1Free;
+
 
     int NextPosIndex;
     Transform NextPos;
 
+    public int posi;
+
 
     void Start()
     {
-        NextPos = Positions[0];
+        posi = gc.pos;
+        NextPos = Positions[gc.pos];
+        //NextPos = gc.Positions[gc.pos];
+
+
+        pos3Free = false;
+        pos2Free = false;
+        pos1Free = false;
+
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("nextposindex: "+NextPosIndex);
-        Debug.Log("nextpos"+ NextPos);
+        //Debug.Log("xexexe: " + gc.pos);
+        //Debug.Log("eeee: " + gc.guestList.IndexOf(gameObject));
+        //Debug.Log("position1: "+pos1Free);
+        //Debug.Log("position2: " + pos2Free);
+        //Debug.Log("posindex:  " + NextPosIndex);
+        //Debug.Log("posindexposition:  " + NextPos.position.x);
+        //Debug.Log("nextposindex: " + NextPosIndex);
+        //Debug.Log("nextpos" + NextPos);
+        //Debug.Log("posit: " + NextPos.position.x);
         MoveGameObject();
+        
+
+
+        //Debug.Log("djasjdhksa: " + NextPos.position.x);
     }
 
 
-    void MoveGameObject()
+    public void MoveGameObject()//Transform transform)
     {
-        if(transform.position.x == NextPos.position.x) //)
+        //this.NextPosIndex = nextPosindex;
+
+
+
+        //NextPos = Positions[NextPosIndex];
+        //NextPos = Positions[gc.guestList.IndexOf(gameObject)];
+
+
+
+
+        //if (pos1Free)
+        //{
+        //    NextPosIndex=2;
+        //}
+
+        //if (pos2Free)
+        //{
+        //    NextPosIndex = 1;
+        //}
+
+        //if (pos3Free)
+        //{
+        //    NextPosIndex = 0;
+        //}
+
+
+        //if (pos1Free == true)
+        //    {
+        //        //Debug.Log("moving1...");
+        //        transform.position = Vector3.MoveTowards(transform.position, NextPos.position, ObjectSpeed * Time.deltaTime);
+        //    }
+
+        //if (pos2Free == true)
+        //{
+        //    //Debug.Log("moving2...");
+        //    transform.position = Vector3.MoveTowards(transform.position, NextPos.position, ObjectSpeed * Time.deltaTime);
+        //}
+
+        //if (pos3Free == true)
+        //{
+        //    //Debug.Log("moving3...");
+        //    transform.position = Vector3.MoveTowards(transform.position, NextPos.position, ObjectSpeed * Time.deltaTime);
+        //}
+
+
+        //if(transform.position.x == NextPos.position.x && transform.position.z == NextPos.position.z)
+        //{
+        //    pos1Free = false;
+        //    pos2Free = true;
+        //}
+
+
+
+
+        if (transform.position.x == NextPos.position.x) //)
         {
 
-            if (Input.GetKeyDown(KeyCode.N))
-            NextPosIndex++;
+            //if (Input.GetKeyDown(KeyCode.N))
+                //NextPosIndex++;
 
-            if(NextPosIndex >= Positions.Length)
+            if (NextPosIndex >= Positions.Count)
             {
                 NextPosIndex = 0;
             }
@@ -42,11 +130,17 @@ public class CashQueue : MonoBehaviour
             if (transform.position.x == Positions[5].position.x)
                 Destroy(gameObject);
 
-            NextPos = Positions[NextPosIndex];
+            //NextPos = Positions[NextPosIndex];
+
+
         }
         else
         {
             transform.position = Vector3.MoveTowards(transform.position, NextPos.position, ObjectSpeed * Time.deltaTime);
         }
+
+
     }
+
+   
 }
---
+++ Customer/globalCustomer.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.ParticleSystem;
+
+public class globalCustomer : MonoBehaviour
+{
+
+    [SerializeField] public Transform[] Positions;
+    //public bool pos3Free;
+    //public bool pos2Free;
+    //public bool pos1Free;
+
+
+    //private SpawnCustomer spawnCustomer;
+    //public GameObject customer;
+    //private GameObject[] customerList;
+
+    [SerializeField] CashQueue cq;
+    public List<GameObject> guestList =  new List<GameObject>();
+    GameObject[] gs;
+
+    public int pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pos3Free = false;
+        //pos2Free = false;
+        //pos1Free = true;
+
+        //GameObject[] gs = GameObject.FindGameObjectsWithTag("Customer");
+
+        //foreach(GameObject g in gs)
+        //{
+        //    Debug.Log("customers: " + g.name);
+        //}
+        //customer = GameObject.FindGameObjectWithTag("Customer");
+        //Debug.Log("customer: " + customer);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //CanAddGuest();
+        //AddGuest(gameObject);
+        //ebug.Log("addguest: " + cq.Positions[guestList.IndexOf(gameObject)]);
+
+        
+
+        gs = GameObject.FindGameObjectsWithTag("Customer");
+
+        foreach (GameObject g in gs)
+        {
+            //Debug.Log("length: " + gs.Length);
+            if(!guestList.Contains(g.gameObject))
+            guestList.Add(g.gameObject);
+            pos = guestList.IndexOf(g.gameObject);
+            Debug.Log("pos: " + pos);
+            //Debug.Log("wew: " + guestList.IndexOf(g.gameObject));
+            //Debug.Log("customers: " + g.name);
+            //Debug.Log("length: " + gs.Length);
+           // Debug.Log("length1: " + guestList.Count);
+            //Debug.Log("length2: " + guestList.Count.ToString());
+            //Debug.Log("position: "+ g.gameObject.)
+        }
+        
+
+        //if (gs[0].transform.position.x == cq.NextPos.position.x && transform.position.z == cq.NextPos.position.z)
+        //{
+        //    cq.pos1Free = false;
+        //    cq.pos2Free = true;
+        //}
+
+        //Debug.Log("x1"+guestList.Count.ToString());
+        //Debug.Log("x2" + gs.Length);
+        //Debug.Log(guestList.Count);
+        //Debug.Log(guestList.ToString());
+
+
+        //if (gs[0].gameObject.transform.position.x)//cq.NextPos.position.x)
+        //{
+
+        //    Debug.Log("xxx");
+
+        //}
+
+        //Debug.Log("x1"+gs[0].gameObject.transform.position.x);
+        //Debug.Log("x2"+gs[0].gameObject.transform.position.x.ToString());
+        //Debug.Log("cq: " + Positions[0].position.x);
+
+
+
+
+        //Debug.Log("customer: " + customer);
+    }
+
+
+    //public bool CanAddGuest()
+    //{
+
+    //    return guestList.Count < cq.Positions.Count;
+    //}
+
+
+    public void AddGuest(GameObject guest)
+    {
+        
+        //cq.MoveGameObject(cq.Positions[guestList.IndexOf(guest)]);
+    }
+
+
+}
