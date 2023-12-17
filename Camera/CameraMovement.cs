a6dbece8db4cc3119b547723c710bcd5561df62e
Anton1812 <s21581@pjwstk.edu.pl>
2023-12-17 22:13:44 +0100

    [+] Camera Movement added
    The script should be attached to the main camera, and in unity the main camera should be attached to the game object in the script

--- Camera/CameraMovement.cs
+++ Camera/CameraMovement.cs
@@ -1,46 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Vector3 Origin;
-    private Vector3 Difference;
-    private Vector3 ResetCamera;
 
-    private bool drag = false;
+    public GameObject camera;
 
-    private void Start()
+    private void Update()
     {
-        ResetCamera = Camera.main.transform.position;
-        
+
+        Vector3 inputDir = new Vector3(0, 0, 0);
+
+        int edgeScrollSize = 10;
+        float moveSpeed = 8f;
+
+        if (camera.transform.position.x < 315)
+        {
+            if (Input.mousePosition.x > Screen.width - edgeScrollSize) inputDir.x = +1f;
+
+        }
+
+        if (camera.transform.position.x > 300)
+        {
+            if (Input.mousePosition.x < edgeScrollSize) inputDir.x = -1f;
+
+        }
+
+        if (camera.transform.position.y > 10)
+        {
+            if (Input.mousePosition.y > Screen.height - edgeScrollSize) inputDir.z = +1f;
+
+        }
+
+        if (camera.transform.position.y < 15)
+        {
+            if (Input.mousePosition.y < edgeScrollSize) inputDir.z = -1f;
+
+        }
+
+        Vector3 moveDir = transform.forward * inputDir.z + transform.right * inputDir.x;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
     }
-
-    private void LateUpdate()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            Difference = Camera.main.ScreenToViewportPoint(Input.mousePosition) - Camera.main.transform.position;
-            if(drag== false)
-            {
-                drag = true;
-                Origin = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-            }
-        }
-        else
-        {
-            drag = false;
-        }
-
-        if (drag)
-        {
-            Camera.main.transform.position = Origin - Difference;
-        }
-
-        if (Input.GetMouseButton(1))
-            Camera.main.transform.position = ResetCamera;
-    }
-
-
 }
